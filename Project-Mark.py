1f527eb4be2340e3528065e82866c2478d00c5c4
Anish Raghavendra <anishr890@gmail.com>
2020-11-18 11:04:18 +0530

    Version 3 first commit

--- Project-Mark.py
+++ Project-Mark.py
@@ -13,3 +13,17 @@ l = '''
              ██║ ╚═╝ ██║██║  ██║██║  ██║██║  ██╗
              ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
 '''
+import io
+import json
+
+from snips_nlu import SnipsNLUEngine
+from snips_nlu.default_configs import CONFIG_EN
+
+engine = SnipsNLUEngine(config=CONFIG_EN)
+
+with io.open("dataset.json") as f:
+    dataset = json.load(f)
+
+engine.fit(dataset)
+parsing = engine.parse("Hey, lights on in the lounge !")
+print(json.dumps(parsing, indent=2))
