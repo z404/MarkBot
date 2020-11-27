75b4b3ea4e5d983b616c75786da6e3dc8f608616
z404 <anishr890@gmail.com>
2020-11-27 09:32:38 +0530

    Added Hardcoded intent

--- PROJECT-MARK/OUTPUT/Greet.py
+++ PROJECT-MARK/OUTPUT/Greet.py
@@ -1,5 +1,8 @@
 import random
 def output(intentname):
     if intentname[1] == 'Normal':
-        a = ['Hi','Hello','Hello there, General Kenobi']
-        return random.choice(a)
+        outputchoice = ['Hi','Hello','Hello there, General Kenobi']
+        return random.choice(outputchoice)
+    if intentname[1] == 'WithQuestion':
+        outputchoice = ['I\'m great! Thank\'s for asking :)','I\'m fantastic!' ]
+        return random.choice(outputchoice)
--- PROJECT-MARK/OUTPUT/IntentAssesment.py
+++ PROJECT-MARK/OUTPUT/IntentAssesment.py
@@ -3,8 +3,10 @@ def output(intent):
     try:
         intentname = intent['intentName'].split('_')
         mod = __import__('OUTPUT.'+intentname[0])
+        print(intent)
         func = getattr(mod,intentname[0])
         return func.output(intentname)
     except ModuleNotFoundError:
         return 'Sorry, I couldn\'t understand that.'
-   
+    except AttributeError:
+        return 'Sorry, I couldn\'t understand that.'
