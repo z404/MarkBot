84e022f1f0da204b676e31c4cde411f401ee5547
Anish Raghavendra <49672329+z404@users.noreply.github.com>
2021-09-01 11:25:44 +0530

    added activity slash commands

--- MARKBOT/MarkBot-v2/cogs/MarkBot-Activity/index.js
+++ MARKBOT/MarkBot-v2/cogs/MarkBot-Activity/index.js
@@ -13,11 +13,137 @@ var request = require("request-promise");
 let rawdata = fs.readFileSync("config.json");
 let config_vars = JSON.parse(rawdata);
 
-client.on("ready", () => {
+const slash_command_data = {
+  name: "activity",
+  description: "Starts a new activity in your voice channel!",
+  options: [
+    {
+      name: "activity_name",
+      type: "STRING",
+      description: "The activity you want to start",
+      required: true,
+      choices: [
+        {
+          name: "Chess",
+          value: "chess",
+        },
+        {
+          name: "Poker",
+          value: "poker",
+        },
+        {
+          name: "Youtube",
+          value: "youtube",
+        },
+        {
+          name: "Betrayal.io",
+          value: "betrayal",
+        },
+        {
+          name: "Fishington.io",
+          value: "fishing",
+        },
+      ],
+    },
+  ],
+};
+
+client.on("ready", async () => {
+  setTimeout(async () => {
+    await client.application?.commands.create(slash_command_data);
+  }, 10000);
   console.log("Activity commands loaded");
 });
 
 client.discordTogether = new DiscordTogether(client);
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === "activity") {
+    const activity_name = interaction.options.get("activity_name")["value"];
+    if (interaction.member.voice.channel) {
+      await interaction.reply("Hold on a sec, making the invite..");
+      if (activity_name === "youtube") {
+        client.discordTogether
+          .createTogetherCode(interaction.member.voice.channel.id, "youtube")
+          .then(async (invite) => {
+            const button = new Discord.MessageButton()
+              .setStyle("LINK")
+              .setURL(invite.code)
+              .setLabel("Click me for Youtube Together!");
+            const row = new Discord.MessageActionRow().addComponents([button]);
+            await interaction.editReply({
+              content:
+                "Click the below button to join the Youtube Together activity!",
+              components: [row],
+            });
+          });
+      } else if (activity_name === "poker") {
+        client.discordTogether
+          .createTogetherCode(interaction.member.voice.channel.id, "poker")
+          .then(async (invite) => {
+            const button = new Discord.MessageButton()
+              .setStyle("LINK")
+              .setURL(invite.code)
+              .setLabel("Click me for Poker Night!");
+            const row = new Discord.MessageActionRow().addComponents([button]);
+            await interaction.editReply({
+              content:
+                "Click the below button to join the Poker Night activity!",
+              components: [row],
+            });
+          });
+      } else if (activity_name === "chess") {
+        client.discordTogether
+          .createTogetherCode(interaction.member.voice.channel.id, "chess")
+          .then(async (invite) => {
+            const button = new Discord.MessageButton()
+              .setStyle("LINK")
+              .setURL(invite.code)
+              .setLabel("Click me for Chess in the park!");
+            const row = new Discord.MessageActionRow().addComponents([button]);
+            await interaction.editReply({
+              content:
+                "Click the below button to join the Chess in the park activity!",
+              components: [row],
+            });
+          });
+      } else if (activity_name === "fishing") {
+        client.discordTogether
+          .createTogetherCode(interaction.member.voice.channel.id, "fishing")
+          .then(async (invite) => {
+            const button = new Discord.MessageButton()
+              .setStyle("LINK")
+              .setURL(invite.code)
+              .setLabel("Click me for Fishington.io!");
+            const row = new Discord.MessageActionRow().addComponents([button]);
+            await interaction.editReply({
+              content:
+                "Click the below button to join the Fishington.io activity!",
+              components: [row],
+            });
+          });
+      } else if (activity_name === "betrayal") {
+        client.discordTogether
+          .createTogetherCode(interaction.member.voice.channel.id, "betrayal")
+          .then(async (invite) => {
+            const button = new Discord.MessageButton()
+              .setStyle("LINK")
+              .setURL(invite.code)
+              .setLabel("Click me for Betrayal!");
+            const row = new Discord.MessageActionRow().addComponents([button]);
+            await interaction.editReply({
+              content: "Click the below button to join the Betrayal activity!",
+              components: [row],
+            });
+          });
+      }
+    } else {
+      await interaction.reply("You aren't in a voice channel!");
+    }
+  }
+});
 
 client.on("messageCreate", async (message) => {
   if (message.content === config_vars["prefix"] + "activity youtube") {
