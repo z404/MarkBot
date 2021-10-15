d0d6b4129eb02eb1f57cfbc8880405790199877f
Anish Raghavendra <49672329+z404@users.noreply.github.com>
2021-10-15 10:00:59 +0530

    added song list for playlist

--- MARKBOT/MarkBot-v2/cogs/Music.py
+++ MARKBOT/MarkBot-v2/cogs/Music.py
@@ -12,6 +12,7 @@ import youtube_dl
 # importing packages for spotify
 import spotipy
 import spotipy.oauth2 as oauth2
+from tabulate import tabulate
 
 
 # Function to write into database
@@ -515,7 +516,7 @@ class Music(commands.Cog):
         if not ctx.voice_state.is_playing:
             return await ctx.send('Not playing any music right now...')
 
-        #voter = ctx.message.author
+        # voter = ctx.message.author
         # if voter == ctx.voice_state.current.requester:
         await ctx.message.add_reaction('⏭')
         ctx.voice_state.skip()
@@ -967,6 +968,33 @@ class Music(commands.Cog):
                 for i in self.db[str(ctx.author.id) + "_saved_playlists"][name_of_playlist]:
                     await self._play(ctx=ctx, search=i, flag=True)
                 await ctx.send("Playlist enqueued!")
+            else:
+                await ctx.send("This playlist has no songs. Please add a few songs first")
+
+    @_playlist.command(pass_context=True, name="listsongs")
+    async def _playlist_listsongs(self, ctx: commands.Context, name_of_playlist: str, pagenumber: int = None):
+        self.db = get_db()
+        if pagenumber == None:
+            pagenumber = 1
+        flag = True
+        try:
+            self.db[str(ctx.author.id) + "_saved_playlists"][name_of_playlist]
+        except KeyError:
+            flag = False
+            await ctx.send("Playlist couldn't be found! Check for typos or create a playlist!")
+        if flag:
+            if self.db[str(ctx.author.id) + "_saved_playlists"][name_of_playlist] != None:
+                embed = discord.Embed(
+                    title="Tracks in "+name_of_playlist, description=ctx.author.mention+"'s playlist contains the following songs")
+                lst = self.db[str(ctx.author.id) +
+                              "_saved_playlists"][name_of_playlist]
+                lst = [[i+1, lst[i]]
+                       for i in range(len(lst))][(pagenumber-1)*10:((pagenumber-1)*10)+10]
+                embed.add_field(name="Tracks", value="```"+tabulate(
+                    lst, tablefmt="fancy_grid")+"```")
+                embed.set_footer(text="Page "+str(pagenumber)+" of "+str((len(self.db[str(ctx.author.id) +
+                                                                                      "_saved_playlists"][name_of_playlist])+9)//10))
+                await ctx.send(embed=embed)
             else:
                 await ctx.send("This playlist has no songs. Please add a few songs first")
 
