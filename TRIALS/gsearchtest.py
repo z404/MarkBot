1e712e250ec03a35a7c41756d8f47308e7f125c1
z404 <anishr890@gmail.com>
2025-03-28 22:59:09 +0530

    chore: remove useless files

--- TRIALS/gsearchtest.py
+++
@@ -1,7 +0,0 @@
-from search_engine_parser import GoogleSearch
-
-search_results = GoogleSearch()
-search_results = search_results.search("itzcozofzak")
-for i in search_results:
-    print(i['titles'])
-print(search_results)
--- TRIALS/replitdbtrial.py
+++
@@ -1,4 +0,0 @@
-from replit import db
-db['Anish'] = 'hello'
-print(db['Anish'])
-del db['Anish']
